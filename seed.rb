03334989017c2f21bc3b0fd6bc9aa51cb3601d1a
Dakota Schramm <william.schramm11@gmail.com>
2024-05-04 09:16:28 -0500

    Add heights and weights for characters

--- seed.rb
+++ seed.rb
@@ -6,27 +6,36 @@ def seed_database(db_name)
   conn = connect_to_database(db_name)
   # Seed the database with initial data
   # You can execute SQL commands to insert data here
-  conn.exec("CREATE TABLE IF NOT EXISTS characters (id SERIAL PRIMARY KEY, name VARCHAR(50))")
-  conn.exec("INSERT INTO characters (name)
+  conn.exec("CREATE TABLE IF NOT EXISTS characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    height SMALLINT, 
+    weight SMALLINT)
+  ")
+  conn.exec("INSERT INTO characters (name, height, weight)
              VALUES
-             ('Blanka'),
-             ('Cammy'),
-             ('Chun-Li'),
-             ('Dee Jay'),
-             ('Dhalsim'),
-             ('E Honda'),
-             ('Guile'),
-             ('Jamie'),
-             ('JP'),
-             ('Juri'),
-             ('Ken'),
-             ('Kimberly'),
-             ('Lily'),
-             ('Luke'),
-             ('Manon'),
-             ('Marisa'),
-             ('Ryu'),
-             ('Zangief')
+             ('Blanka', 76, 216),
+             ('Cammy', 65, 134),
+             ('Chun-Li', 67, 0),
+             ('Dee Jay', 72, 203),
+             ('Dhalsim', 69, 106),
+             ('E Honda', 73, 302),
+             ('Guile', 72, 218),
+             ('Jamie', 69, 170),
+             ('JP', 75, 214),
+             ('Juri', 65, 126),
+             ('Ken', 69, 183),
+             ('Kimberly', 66, 134),
+             ('Lily', 63, 106),
+             ('Luke', 73, 198),
+             ('Manon', 69, 129),
+             ('Marisa', 80, 269),
+             ('Ryu', 69, 187),
+             ('Zangief', 84, 399),
+             ('Rashid', 70, 187),
+             ('A.K.I.', 68, 126),
+             ('Ed', 72, 190),
+             ('Akuma', 70, 198)
             ")
   # Add more seed data as needed
 
